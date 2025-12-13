ba9a611d4bc5e95e6e76adbdb07d9d00f8ae1b9e
prabh16 <prabhnoor43991@gmail.com>
2025-12-13 22:09:11 +0530

    feat: add sweets post endpoint

--- backend/src/controllers/sweets.controller.js
+++ backend/src/controllers/sweets.controller.js
@@ -2,6 +2,11 @@ const getAllSweets = (req, res) => {
   return res.status(200).json([]);
 };
 
+const addSweet = (req, res) => {
+  return res.status(201).json({ message: "Sweet added" });
+};
+
+
 module.exports = {
-  getAllSweets,
+  getAllSweets, addSweet,
 };
--- backend/src/routes/sweets.routes.js
+++ backend/src/routes/sweets.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getAllSweets} = require("../controllers/sweets.controller");
+const {getAllSweets, addSweet} = require("../controllers/sweets.controller");
 
 router.get("/", getAllSweets);
+router.post("/", addSweet);
 
 module.exports = router;
