ba5b49d6148a68f461419db6ae8eec8aa3ede80b
prabh16 <prabhnoor43991@gmail.com>
2025-12-13 19:16:45 +0530

    test: fail registration when email is missing

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -11,3 +11,14 @@ test("User registration works", async() => {
 
     expect(res.statusCode).toBe(201);
 });
+
+
+test("User registration fails when email is missing", async() =>{
+    const res = await request(app)
+    .post("/api/auth/register")
+    .send({
+        password: "100000";
+    });
+
+    expect(res.statusCode).toBe(400);
+});
