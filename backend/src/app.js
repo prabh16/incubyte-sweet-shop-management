f26b9a18c8d7466e40867f80cabead5a10979f13
prabh16 <prabhnoor43991@gmail.com>
2025-12-13 22:03:36 +0530

    feat: return empty sweets list

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 
 const authRoutes = require("./routes/auth.routes");
+const sweetsRoutes = require("./routes/sweets.routes");
+
 app.use("/api/auth", authRoutes);
+app.use("/api/sweets", sweetsRoutes);
 
 app.get("/", (req, res) => {
     res.json({message: "Sweet Shop Management API running"});
