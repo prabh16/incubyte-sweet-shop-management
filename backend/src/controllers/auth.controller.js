1f4633fb0cbb307c4a17bf7c91c6dda86dbd9bdd
prabh16 <prabhnoor43991@gmail.com>
2025-12-13 21:32:25 +0530

    feat: return 400 when password is too short

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -7,6 +7,10 @@ const validateRegisterInput = ({email,password}) =>{
         return "Password is required"; 
     }
 
+    if(password.length < 6){
+        return "Password must be at least 6 characters long";
+    }
+
     return null;
 };
 
