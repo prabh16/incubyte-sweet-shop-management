cba505bce3f3009d347b8f14227096ceddf8aacc
prabh16 <prabhnoor43991@gmail.com>
2025-12-13 22:10:56 +0530

    refactor: prepare sweets post payload mapping

--- backend/src/controllers/sweets.controller.js
+++ backend/src/controllers/sweets.controller.js
@@ -1,8 +1,14 @@
+const mapSweetPayload = (body) => {
+  const { name, price } = body;
+  return { name, price };
+};
+
 const getAllSweets = (req, res) => {
   return res.status(200).json([]);
 };
 
 const addSweet = (req, res) => {
+  mapSweetPayload(req.body);
   return res.status(201).json({ message: "Sweet added" });
 };
 
