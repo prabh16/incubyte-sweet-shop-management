1b1e06596cd50bcb8304df37d9b974617a22f780
prabh16 <prabhnoor43991@gmail.com>
2025-12-13 22:01:12 +0530

    test: fail when fetching sweets initially

--- backend/tests/sweets.test.js
+++ backend/tests/sweets.test.js
@@ -1,3 +1,10 @@
-test.skip("Sweets tests will be added later on", () =>{
+const request = require("supertest");
+const app = require("../src/app");
 
+test("Get all sweets returns empty array initially", async () => {
+  const res = await request(app).get("/api/sweets");
+
+  expect(res.statusCode).toBe(200);
+  expect(Array.isArray(res.body)).toBe(true);
+  expect(res.body.length).toBe(0);
 });
